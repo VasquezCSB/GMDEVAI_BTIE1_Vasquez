963d7f6471f9e4b710fa448dc93098206c2ef1e7
Ryan Lorenzo Vasquez <ryanlorenzo.vasquez@benilde.edu.ph>
2023-07-29 16:02:39 +0800

    M5 Committed

---
+++ GMDEVAI_M5_Vasquez/Assets/Scripts/Zombie2_Script.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Zombie2_Script : MonoBehaviour
+{
+    NavMeshAgent agent;
+    public GameObject target;
+    public WASDMovement playerMovement;
+
+    Vector3 wanderTarget;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = this.GetComponent<NavMeshAgent>();
+        playerMovement = target.GetComponent<WASDMovement>();
+    }
+
+    private void Seek(Vector3 location)
+    {
+        agent.SetDestination(location);
+    }
+
+    void Wander()
+    {
+        float wanderRadius = 20;
+        float wanderDistance = 10;
+        float wanderJitter = 1;
+
+        wanderTarget += new Vector3(Random.Range(-1.0f, 1.0f) * wanderJitter,
+                        0, Random.Range(-1.0f, 1.0f) * wanderJitter);
+
+        wanderTarget.Normalize();
+        wanderTarget *= wanderRadius;
+
+        Vector3 targetLocal = wanderTarget + new Vector3(0, 0, wanderDistance);
+        Vector3 targetWorld = this.gameObject.transform.InverseTransformVector(targetLocal);
+
+        Seek(targetWorld);
+    }
+
+    void Hide()
+    {
+        float distance = Mathf.Infinity;
+        Vector3 chosenSpot = Vector3.zero;
+
+        int hidingSpotsCount = World.Instance.GetHidingSpots().Length;
+        for (int i = 0; i < hidingSpotsCount; i++)
+        {
+            Vector3 hideDirection = World.Instance.GetHidingSpots()[i].transform.position - target.transform.position;
+            Vector3 hidePosition = World.Instance.GetHidingSpots()[i].transform.position + hideDirection.normalized * 5;
+
+            float spotDistance = Vector3.Distance(this.transform.position, hidePosition);
+            if (spotDistance < distance)
+            {
+                chosenSpot = hidePosition;
+                distance = spotDistance;
+            }
+        }
+
+        Seek(chosenSpot);
+    }
+    bool canSeeTarget()
+    {
+        RaycastHit raycastInfo;
+        Vector3 rayToTarget = target.transform.position - this.transform.position;
+        if (Physics.Raycast(this.transform.position, rayToTarget, out raycastInfo))
+        {
+            return raycastInfo.transform.gameObject.tag == "Player";
+        }
+
+        return false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (canSeeTarget() == true)
+        {
+            Hide();
+        }
+        else
+        {
+            Wander();
+        }
+    }
+}
